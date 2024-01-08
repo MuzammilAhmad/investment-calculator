d6024f70842b3857c16b7a476e441bbb5237a792
CodeSandbox <support@codesandbox.io>
2024-01-08 11:08:43 +0000

    Initial commit

---
+++ src/components/UserInput.jsx
@@ -0,0 +1,51 @@
+export default function UserInput({ onChange, userInput }) {
+  return (
+    <section id="user-input">
+      <div class="input-group">
+        <p>
+          <label>Initial Investment</label>
+          <input
+            type="number"
+            required
+            value={userInput.initialInvestment}
+            onChange={(event) =>
+              onChange("initialInvestment", event.target.value)
+            }
+          />
+        </p>
+        <p>
+          <label>Annual Investment</label>
+          <input
+            type="number"
+            required
+            value={userInput.annualInvestment}
+            onChange={(event) =>
+              onChange("annualInvestment", event.target.value)
+            }
+          />
+        </p>
+      </div>
+
+      <div class="input-group">
+        <p>
+          <label>Expected Result</label>
+          <input
+            type="number"
+            required
+            value={userInput.expectedReturn}
+            onChange={(event) => onChange("expectdReturn", event.target.value)}
+          />
+        </p>
+        <p>
+          <label>Duration</label>
+          <input
+            type="number"
+            required
+            value={userInput.duration}
+            onChange={(event) => onChange("duration", event.target.value)}
+          />
+        </p>
+      </div>
+    </section>
+  );
+}
